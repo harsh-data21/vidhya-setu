a1d222ff53159f028e2a5c59b92f4eda7ac3555f
bughunt21 <142666898+bughunt21@users.noreply.github.com>
2025-12-26 22:38:03 +0530

    Day 3: Models, admin testing, basic views and templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+
+    return render(request, 'login.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
